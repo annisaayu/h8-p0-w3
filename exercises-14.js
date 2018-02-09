bc9acd6f969e199dbef7dc02eeb53fcaa00e0a17
annisaayu <annisaayu569@gmail.com>
2018-02-09 08:33:29 +0700

    exercises week 3

---
+++ exercises-14.js
@@ -0,0 +1,28 @@
+/*----------------------
+  MENGELOMPOKKAN ANGKA
+----------------------*/
+
+function mengelompokkanAngka(arr) {
+  var ganjil=[];
+  var genap=[];
+  var kelipatan3=[];
+  var gabung=[];
+  for(var i=0; i<arr.length;i++){
+
+    if(arr[i]%3===0){
+      kelipatan3.push(arr[i]);
+    }else if(arr[i]%2===0){
+      genap.push(arr[i]);
+    }else{
+      ganjil.push(arr[i]);
+    }
+  }
+  gabung.push(genap, ganjil, kelipatan3);
+  return gabung;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
